2066671f74d107c27bdfc4ff25655e35bf9eeef8
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-01-04 17:43:02 +0700

    cek untuk voice to text

--- app/src/main/java/it/yudharta/smartkalkulator/MainActivity.java
+++ app/src/main/java/it/yudharta/smartkalkulator/MainActivity.java
@@ -1,15 +1,24 @@
 package it.yudharta.smartkalkulator;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText nilai1, nilai2, hasil;
     Button btn;
+    TextView tvVoice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         hasil  =  (EditText) findViewById(R.id.edhasil);
 
         btn = (Button) findViewById(R.id.button);
+
+        tvVoice = (TextView) findViewById(R.id.tvVoice);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,5 +51,29 @@ public class MainActivity extends AppCompatActivity {
 
     public int kurang(int nil1, int nil2) {
         return nil1 - nil2;
+    }
+
+    public void clickVoice(View v) {
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "ucapkan sesuatu");
+
+        try {
+            startActivityForResult(i,100);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "Suara Tidak didukung perangkat ini", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case 100 : if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                tvVoice.setText(result.get(0));
+            }
+        }
     }
 }
